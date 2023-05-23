5231c3167eb857d6b27bcebf480e4e4bdbe3a0af
handitan <handi.tan@gmail.com>
2023-05-23 14:25:51 -0500

    Added dialog prompt for appt deletion

--- app/src/main/java/com/handitan/personalappointmentscheduler/presentation/ui/appointmentList/components/AppointmentCard.kt
+++ app/src/main/java/com/handitan/personalappointmentscheduler/presentation/ui/appointmentList/components/AppointmentCard.kt
@@ -7,13 +7,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.Icon
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.handitan.personalappointmentscheduler.core.Utilities
@@ -25,6 +31,8 @@ fun AppointmentCard(
     deleteAppt:(AppointmentData)->Unit,
     navigateToUpdateApptScreen:(Long)->Unit
 ) {
+    var showDeleteApptDialog by remember { mutableStateOf(false) }
+
     Card(modifier = Modifier
         .fillMaxWidth()
         .padding(15.dp)
@@ -40,7 +48,7 @@ fun AppointmentCard(
             Text(text = currentAppt.hour.toString() + ":" + currentAppt.minute.toString())
             IconButton(
                 onClick = {
-                    deleteAppt(currentAppt)
+                    showDeleteApptDialog = true
                 }
             ) {
                 Icon(
@@ -49,5 +57,56 @@ fun AppointmentCard(
                 )
             }
         }
+    }
+
+    if (showDeleteApptDialog) {
+        DeleteConfirmationAlertDialog(
+            currentAppt,
+            deleteAppt
+        ) {
+            showDeleteApptDialog = false
+        }
+    }
+}
+
+@Composable
+fun DeleteConfirmationAlertDialog(
+    currentAppt:AppointmentData,
+    deleteAppt:(AppointmentData)->Unit,
+    resetShowDeleteApptDialogState:()->Unit
+) {
+    val openDialog = remember {mutableStateOf(true)}
+
+    val handleDialogClosed = {
+        openDialog.value = false
+        resetShowDeleteApptDialogState()
+    }
+
+    if (openDialog.value) {
+        AlertDialog(
+            onDismissRequest = {
+                handleDialogClosed()
+            },
+            title = {
+                Text(text = "Delete")
+            },
+            text = {
+                Text(text = "Continue to delete this appointment?")
+            },
+            confirmButton = {
+                TextButton(onClick = {
+                    openDialog.value = false
+                    deleteAppt(currentAppt)
+                }) {
+                    Text(text = "Confirm")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = {
+                    handleDialogClosed()
+                }) {
+                    Text(text = "Cancel")
+                }
+            })
     }
 }
