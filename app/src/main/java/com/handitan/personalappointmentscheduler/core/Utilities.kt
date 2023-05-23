9673532d17009bc3accfb9940fc8d8d984bdf2e5
handitan <handi.tan@gmail.com>
2023-05-23 15:29:17 -0500

    Fixed date selected off by 1 day

--- app/src/main/java/com/handitan/personalappointmentscheduler/core/Utilities.kt
+++ app/src/main/java/com/handitan/personalappointmentscheduler/core/Utilities.kt
@@ -2,11 +2,14 @@ package com.handitan.personalappointmentscheduler.core
 
 import java.text.SimpleDateFormat
 import java.util.Locale
+import java.util.TimeZone
 
 class Utilities {
     companion object {
         fun changeToDateString(dateVal:Long):String {
-            val sdf = SimpleDateFormat("MM/dd/yyyy", Locale.getDefault())
+            val sdf = SimpleDateFormat("MM/dd/yyyy", Locale.getDefault()).apply {
+                timeZone = TimeZone.getTimeZone("UTC")
+            }
             return sdf.format(dateVal)
         }
     }
