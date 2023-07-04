93efbb4162083ca659da79b730a5b0a0c703b85e
handitan <handi.tan@gmail.com>
2023-07-04 18:21:25 -0500

    Added fillMaxSize for column

--- app/src/main/java/com/handitan/personalappointmentscheduler/presentation/ui/appointmentList/components/AppointmentCard.kt
+++ app/src/main/java/com/handitan/personalappointmentscheduler/presentation/ui/appointmentList/components/AppointmentCard.kt
@@ -1,14 +1,18 @@
 package com.handitan.personalappointmentscheduler.presentation.ui.appointmentList.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.handitan.personalappointmentscheduler.core.Utilities
@@ -27,7 +31,10 @@ fun AppointmentCard(
         },
         elevation = CardDefaults.cardElevation()
     ) {
-        Column(modifier = Modifier.padding(10.dp)) {
+        Column(modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp),
+        ) {
             Text(text = currentAppt.description,
             maxLines = 2,
             overflow = TextOverflow.Ellipsis)
