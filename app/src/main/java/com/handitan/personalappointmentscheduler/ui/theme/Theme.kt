f180a9782ab2e89cb5ddb7f7dbd8de60039da6b6
handitan <handi.tan@gmail.com>
2023-07-04 18:14:24 -0500

    Added theme and color. Set to darkColorScheme

--- app/src/main/java/com/handitan/personalappointmentscheduler/ui/theme/Theme.kt
+++ app/src/main/java/com/handitan/personalappointmentscheduler/ui/theme/Theme.kt
@@ -10,31 +10,52 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = RedOrange80,
+    primaryContainer = RedOrange75,
+    surfaceVariant = Pink30,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = RedOrange80,
+    primaryContainer = RedOrange75,
+    surfaceVariant = Pink30,
+//    onPrimary = Color.White,
+//    onSurfaceVariant = Color.White,
+//    background = Color.Black,
+//    onBackground = Color.White,
+//    primary = Green40,
+//    onPrimary = Color.White,
+//    primaryContainer = Green90,
+//    onPrimaryContainer = Green10,
+//    inversePrimary = Green80,
+//    secondary = DarkGreen40,
+//    onSecondary = Color.White,
+//    secondaryContainer = DarkGreen90,
+//    onSecondaryContainer = DarkGreen10,
+//    tertiary = Violet40,
+//    onTertiary = Color.White,
+//    tertiaryContainer = Violet90,
+//    onTertiaryContainer = Violet10,
+//    error = Red40,
+//    onError = Color.White,
+//    errorContainer = Red90,
+//    onErrorContainer = Red10,
+//    background = Grey99,
+//    onBackground = Grey10,
+//    surface = GreenGrey90,
+//    onSurface = GreenGrey30,
+//    inverseSurface = Grey20,
+//    inverseOnSurface = Grey95,
+//    surfaceVariant = GreenGrey90,
+//    onSurfaceVariant = GreenGrey30,
+//    outline = GreenGrey50
 )
 
 @Composable
@@ -44,15 +65,16 @@ fun PersonalAppointmentSchedulerTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colorScheme = DarkColorScheme
+//        when {
+////        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+////            val context = LocalContext.current
+////            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+////        }
+//
+//        darkTheme -> DarkColorScheme
+//        else -> LightColorScheme
+//    }
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
